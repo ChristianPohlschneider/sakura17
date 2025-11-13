2a7d0f75b8eb5c8e4d24f6054215601435730b2a
ChristianPohlschneider <c.pohlschneider@t-online.de>
2025-11-13 20:08:21 +0100

    added our-menu html scss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContent } from "./main-content/main-content";
+import { Footer } from './shared/components/footer/footer';
+import { Imprint } from './imprint/imprint';
 
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    MainContent
+    MainContent,
+    Footer,
+    Imprint
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
